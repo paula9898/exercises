e86e72f9be818a48a3eb4e6542dadd399013ad61
paula9898 <otrebskapaulina@gmail.com>
2022-03-07 18:37:46 +0100

    first project

---
+++ lesson1.py
@@ -0,0 +1,16 @@
+
+name = 'Paulina'
+surname = 'Otrebska'
+age = 23
+
+print(name, surname, age)
+
+name1 = 'Bob'
+comunicat = 'Buy se glue'
+
+print("Hello, {}, {}". format(name1, comunicat))
+
+print("Hej,", name1 + ".", comunicat)
+
+
+
