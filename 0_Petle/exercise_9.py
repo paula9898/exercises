e08caf89c247a8cd68971d47f165be8805677b7d
paula9898 <otrebskapaulina@gmail.com>
2022-03-21 16:16:57 +0100

    some exercises using loops

--- 0_Petle/exercise_9.py
+++ 0_Petle/exercise_9.py
@@ -5,7 +5,8 @@
 
 # Poproś użytkownika o podanie początkowego poziomu paliwa. Użytkownik ma kontynuować czynność,
 # dopóki nie poda poprawnej wartości - mieszczącej się pomiędzy 5000 a 30000 litrów.
-# Stwórz drugą pętlę, która będzie prosić o użytkownika o podanie odpowiedniej ilości astronautów znajdujących na pokładzie.
+# Stwórz drugą pętlę, która będzie prosić o użytkownika o podanie odpowiedniej
+# ilości astronautów znajdujących na pokładzie.
 # Pętla ma walidować podaną liczbę - tak, aby była dodatnia i nie większa niż 7.
 # Zasymuluj pętlą nr 3 lot statku kosmicznego.
 # Kolejne iteracje mają zmniejszać obecny poziom paliwa o określoną wartość. Zużycie paliwa co 100 km zależy od ilości astronautów na pokładzie i jest równe: 300 l + 100 * ilosc_astronautow.
@@ -18,9 +19,18 @@ while fuelLevel not in range(5000, 30000):
     fuelLevel = int(input("Enter a start fuel level: "))
 
 flag = True
-while True:
+while flag:
     astronauts = int(input("Enter a number of astronauts: "))
-
-    if astronauts in range(0, 7):
+    if astronauts in range(0, 8):
         flag = False
-        print(astronauts)
+print(astronauts)
+
+height = 0
+
+for i in range(fuelLevel, 0, -100):
+    fuelUsage = 300 + 100 * astronauts
+    fuelLevel -= fuelUsage
+    print(fuelLevel)
+    height += 100
+
+print(height)
